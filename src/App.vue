0094ffc17b6e7191026cf6c8232b3883f210923a
Riccardo <riky.volto@live.com>
2023-10-03 17:57:07 +0200

    add main component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
-import AppButton from './components/AppButton.vue'
+import AppMain from './components/AppMain.vue'
 export default {
   name: 'App',
   data() {
@@ -10,54 +10,14 @@ export default {
   },
   components: {
     AppHeader,
-    AppButton,
+    AppMain,
   }
 }
 </script>
 
 <template>
   <AppHeader></AppHeader>
-  <main>
-    <section class="bg-black">
-      <div class="container py-5 w-100 m-0 g-0">
-        <div class="row g-0">
-          <div class="col">
-            <img class="with_600" src="./assets/images/features-illus.png" alt="">
-          </div>
-          <div class="col text-white d-flex flex-column">
-            <h5>The ultimate competitive gaming platform</h5>
-            <h2>Make your mark on the battle field</h2>
-            <p class="w-75">Libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at
-              tellus. Cras ultricies liguia sed magna dictum porta.</p>
-            <div class="d-flex align-items-center">
-              <img src="./assets/images/icon/features-icon-1.png" alt="">
-              <div class="ps-3">
-                <h4 class="text-white">Live streaming</h4>
-                <p>Lorem ipsum dolor sit amet. consectetur adipiscing elit, sed do eismod tempor incididunt ut labor.</p>
-              </div>
-            </div>
-            <div class="d-flex align-items-center">
-              <img src="./assets/images/icon/features-icon-2.png" alt="">
-              <div class="ps-3">
-                <h4 class="text-white">Great Tournament</h4>
-                <p>Lorem ipsum dolor sit amet. consectetur adipiscing elit, sed do eismod tempor incididunt ut labor.</p>
-              </div>
-            </div>
-            <div class="d-flex align-items-center">
-              <img src="./assets/images/icon/features-icon-3.png" alt="">
-              <div class="ps-3">
-                <h4 class="text-white">Gaming News</h4>
-                <p>Lorem ipsum dolor sit amet. consectetur adipiscing elit, sed do eismod tempor incididunt ut labor.</p>
-              </div>
-            </div>
-            <div class="pt-4">
-              <AppButton :buttonText="'Play Now'" :img="'src/assets/images/svg/e-double-right-arrow.svg'"></AppButton>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+  <AppMain></AppMain>
 </template>
 
 <style lang="scss">
@@ -65,12 +25,5 @@ export default {
 
 
 
-.with_600 {
-  width: 600px;
-}
 
-h2 {
-  font-size: 55px;
-  color: white;
-}
 </style>
