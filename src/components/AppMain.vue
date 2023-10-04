5ea84e620d78f2251ee8b5f89a4267191155b88c
Riccardo <riky.volto@live.com>
2023-10-04 18:02:21 +0200

    add gamers say card structure

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -177,6 +177,62 @@ export default {
             </div>
 
         </section>
+
+        <!-- gamers say section -->
+        <section class="bg-black pt-5">
+            <div class="container w-50 text-center">
+                <h5>Futio feedback</h5>
+                <h2>What Our gamers Say</h2>
+                <p class="text-white px-5 pb-5">libero melesuada feugiat. Vivamus magna justo, lacinia eget
+                    consectetur sed,
+                    convallis at tellus. <br> Cras ultricies liguia sed magna dictum porta.
+                </p>
+            </div>
+            <div class="container w-75">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="card p-4 opinion_card">
+                            <div class="d-flex g-0">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-star-fill" viewBox="0 0 16 16">
+                                    <path
+                                        d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                                </svg>
+                            </div>
+                            <p class="py-2">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor."</p>
+                            <div class="d-flex">
+                                <img class=" object-fit-contain" src="../assets/images/user-image-1.png" alt="">
+                                <div class="d-flex flex-column ps-2">
+                                    <div>Kim Ramos</div>
+                                    <div>Serbia</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     </main>
 </template>
 
@@ -273,4 +329,16 @@ h2 {
         }
     }
 }
+
+
+/* gamers say section */
+
+path {
+    color: #f8bb04;
+}
+.opinion_card {
+    border-radius: 20px;
+    background-color: #28344a;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
+import lottie from 'lottie-web';
+import { defineElement } from 'lord-icon-element';
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
 createApp(App).mount('#app')
