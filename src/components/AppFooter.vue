a0ffb2660e4589ce556960c93c1f09e9c3502b96
Riccardo <riky.volto@live.com>
2023-10-05 11:50:02 +0200

    finish footer structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,58 +6,85 @@ export default {
             footerInfos: [
                 {//card1
                     h3: 'Quick Links',
-                    a: ['Games', 'Latest Tournament', 'Shop', 'Blog', 'Contact']
+                    a: ['Games', 'Latest Tournament', 'Shop', 'Blog', 'Contact'],
+
                 },
-             
-            ]
+                {//card2
+                    h3: 'Help',
+                    a: ['Faqs', 'Teams & Conditions', 'Privacy',]
+                },
+                {//card3
+                    h3: 'Follow Us',
+                    a: ['Feedback', 'Twitter', 'Instagram', 'Youtube', 'Github']
+                },
+            ],
+            FooterInfoContacts: [
+                {//card4
+                    h3: 'Contect',
+                    aLocation: ['4nMayfield, Buckden, PE19 5SZ'],
+                    aPhone: ['(302)5550107']
+                },
+            ],
         }
     },
 }
 </script>
 
 <template>
-    <footer class="bg_secondary">
-            <div class="container w-75">
-                <div class="row">
-                    <div class="col-4 d-flex flex-column">
-                        <div>
-                            <img src="../assets/images/header-images/header logo.png" alt="">
-                        </div>
-                        <p class="py-3">Lorem ipsum dolor sit amet,elit,varius enim in eros elementum Duis mi quis viverra ornare</p>
-                        <div class="d-flex">
-                            <div class="d-flex footer_icon">
-                                <img src="../assets/images/icon/youtube.png" alt="">
-                            </div>
-                            <div class="d-flex footer_icon">
-                                <img src="../assets/images/icon/twitch.png" alt="">
-                            </div>
-                            <div class="d-flex footer_icon">
-                                <img src="../assets/images/icon/youtube.png" alt="">
-                            </div>
-                            <div class="d-flex footer_icon">
-                                <img src="../assets/images/icon/twitch.png" alt="">
-                            </div>
-                        </div>
+    <footer class="">
+        <div class="container w-75">
+            <div class="row pb_5rem">
+                <div class="col-4 d-flex flex-column">
+                    <div>
+                        <img src="../assets/images/header-images/header logo.png" alt="">
                     </div>
-                    <div class="col-2">
-                        <div class="d-flex">
-                            <div v-for="footerInfo in footerInfos" class="d-flex flex-column">
-                                <h3>{{ footerInfo.h3 }}</h3>
-                                <a v-for="a in footerInfo.a" href="">{{ a }}</a>
-                            </div>
+                    <p class="py-3">Lorem ipsum dolor sit amet,elit,varius enim in eros elementum Duis mi quis viverra
+                        ornare</p>
+                    <div class="d-flex">
+                        <div class="d-flex footer_icon">
+                            <img src="../assets/images/icon/youtube.png" alt="">
+                        </div>
+                        <div class="d-flex footer_icon">
+                            <img src="../assets/images/icon/twitch.png" alt="">
+                        </div>
+                        <div class="d-flex footer_icon">
+                            <img src="../assets/images/icon/youtube.png" alt="">
+                        </div>
+                        <div class="d-flex footer_icon">
+                            <img src="../assets/images/icon/twitch.png" alt="">
                         </div>
                     </div>
                 </div>
-
+                <div class="col-2" v-for="footerInfo in footerInfos">
+                    <div class="d-flex">
+                        <div class="d-flex flex-column">
+                            <h3>{{ footerInfo.h3 }}</h3>
+                            <a v-for="a in footerInfo.a" href="">{{ a }}</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2" v-for="FooterInfoContact in FooterInfoContacts">
+                    <div class="d-flex">
+                        <div class="d-flex flex-column">
+                            <h3>{{ FooterInfoContact.h3 }}</h3>
+                            <div class="color_gray_fluo">Location:</div>
+                            <a v-for="a in FooterInfoContact.aLocation" href="">{{ a }}</a>
+                            <div class="color_gray_fluo">Phone:</div>
+                            <a v-for="aPhone in FooterInfoContact.aPhone" href="">{{ aPhone }}</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </footer>
+        </div>
+        <div class="copyright_footer d-flex justify-content-center align-items-center">
+            <div class="">Copyright @ 2022 <span class="color_gray_fluo">Futio</span>.All rights Reserve</div>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
-
-.bg_secondary {
+footer {
     background-color: #28344a;
-
 }
 
 h3 {
@@ -67,6 +94,7 @@ h3 {
 p {
     color: white;
 }
+
 .footer_icon {
     border: 1px solid white;
     border-radius: 50%;
@@ -85,7 +113,29 @@ p {
         color: white;
 
     }
+
 }
 
+a {
+    text-decoration: none;
+    color: white;
+    width: fit-content;
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+
+.color_gray_fluo {
+    color: #92cb52;
+}
+
+.pb_5rem {
+    padding-bottom: 5rem;
+}
+
+.copyright_footer {
+    background-color: #1b212d;
+    color: white;
+    height: 80px;
+}
 </style>
   
